3a9a290c6b1973598ab24c7a3d1a798b11dca526
Sardor <34943401+Khuzha@users.noreply.github.com>
2019-06-06 17:40:02 +0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+module.exports = {
+  token: 'string', // you can get it in @BotFather
+  mongoLink: 'string', // mongo-link from cloud.mongodb.com
+  keyboard: {
+    keyboard: [['📈 Statistic', '📁 Source code']], 
+    resize_keyboard: true
+  }
+}
