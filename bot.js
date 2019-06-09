c72c11a483fa18406eaaaf5d27a6d804dca5a671
khuzha <t.sardorkhuja@gmail.com>
2019-06-09 18:05:06 +0500

    refactored the code
    maked separate file for dates calculating

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Telegraf = require('telegraf')
 const mongo = require('mongodb').MongoClient
 const data = require('./data')
 const zodiac = require('./zodiac')
+const calculator = require('./calculator')
 const bot = new Telegraf(data.token)
 
 mongo.connect(data.mongoLink, {useNewUrlParser: true}, (err, client) => {
@@ -15,7 +16,7 @@ mongo.connect(data.mongoLink, {useNewUrlParser: true}, (err, client) => {
 
 bot.start((ctx) => {
   ctx.reply(
-    'Send me your birthday in format DD.MM.YYYY (e.g. 21.12.2000)',
+    'Send me your birthday in format DD.MM.YYYY (e.g. 01.01.1990)',
     { reply_markup: data.keyboard }
   )
   updateUser(ctx, true)
@@ -39,27 +40,11 @@ bot.hears(/^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/, (ctx) => {
     return ctx.reply('Was you born in the future? Please send a correct date.')
   }
   
+  const { 
+    milliseconds, seconds, minutes, hours, days, weeks, months, years,
+    today, thisMonth, thisYear, nowHours, nowMinutes, nowSeconds
+  } = calculator.getAge(date)
   
-  const milliseconds = now - date.getTime()
-  const seconds = Math.floor(milliseconds / 1000)
-  const minutes = Math.floor(seconds / 60)
-  const hours = Math.floor(minutes / 60)
-  const days = Math.floor(hours / 24)
-  const weeks = Math.floor(days / 7)
-  const months = Math.floor(days / 2 / 31 + days / 2 / 30)
-  const years = Math.floor((days - Math.floor(days / 365 / 4)) / 365)
-  
-  now = new Date()
-  let today = commafy(now.getDate())
-  let thisMonth = commafy(now.getMonth() + 1)
-  const thisYear = now.getFullYear()
-
-  today.length == 1 ? today = '0' + today : false
-  thisMonth.length == 1 ? thisMonth = '0' + thisMonth : false
-
-  const nowHours = now.getUTCHours().toString().length == 1 ? '0' + now.getUTCHours() : now.getUTCHours()
-  const nowMinutes = now.getUTCMinutes().toString().length == 1 ? '0' + now.getUTCMinutes() : now.getUTCMinutes()
-  const nowSeconds = now.getUTCSeconds().toString().length == 1 ? '0' + now.getUTCSeconds() : now.getUTCSeconds()
 
   ctx.reply(
     `Now it's ${nowHours}:${nowMinutes}:${nowSeconds} ` + 
@@ -67,8 +52,8 @@ bot.hears(/^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/, (ctx) => {
     `${today}.${thisMonth}.${thisYear}. \nYou were born in ${ctx.message.text}.` +
     `\nYour zodiac sign is ${zodiac.getZodiacSign(textArr[0], textArr[1])}` +
     `\n\nYour life in: \n` + 
-    `\nYears: ${years} \nMonths: ${commafy(months)} \nWeeks: ${commafy(weeks)} \nDays: ${commafy(days)}` +
-    `\nHours: ${commafy(hours)} \nMinutes: ${commafy(minutes)} \nSeconds: ${commafy(seconds)} \nMilliseconds: ${commafy(milliseconds)}`,
+    `\nYears: ${years} \nMonths: ${months} \nWeeks: ${weeks} \nDays: ${days}` +
+    `\nHours: ${hours} \nMinutes: ${minutes} \nSeconds: ${seconds} \nMilliseconds: ${milliseconds}`,
     { 
       reply_markup: {
         inline_keyboard: [[{text: '⤴️ Share', url: `t.me/share/url?url=${encodeURI(`@LifeDaysBot has said that I live already ${commafy(weeks)} weeks, ${commafy(days)} days, ${commafy(hours)} hours etc. Learn, how much days do you live`)}`}]]
@@ -107,7 +92,7 @@ bot.hears('📁 Source code', (ctx) => {
 
 bot.on('message', (ctx) => {
   ctx.reply(
-    'Message you sent me isn`t in correct format. Send me your birthday in format DD.MM.YYYY (e.g. 21.12.2000)',
+    'Message you sent me isn`t in correct format. Send me your birthday in format DD.MM.YYYY (e.g. 01.01.1990)',
     { reply_markup: data.keyboard }
   )
   updateUser(ctx, true)
@@ -125,10 +110,6 @@ updateStat = (action) => {
 
   db.collection('statistic').updateOne({action: action}, {$inc: {[makeDate()]: 1}}, {new: true, upsert: true})
   db.collection('statistic').updateOne({genAct: action}, {$inc: {count: 1}}, {new: true, upsert: true})
-}
-
-commafy = (num) => {
-  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
 }
 
 makeDate = () => {
---
+++ calculator.js
@@ -0,0 +1,48 @@
+getAge = (date) => {
+  let now = Date.now()
+  const milliseconds = now - date.getTime()
+  const seconds = Math.floor(milliseconds / 1000)
+  const minutes = Math.floor(seconds / 60)
+  const hours = Math.floor(minutes / 60)
+  const days = Math.floor(hours / 24)
+  const weeks = Math.floor(days / 7)
+  const months = Math.floor(days / 2 / 31 + days / 2 / 30)
+  const years = Math.floor((days - Math.floor(days / 365 / 4)) / 365)
+  
+  now = new Date()
+  let today = now.getDate()
+  let thisMonth = now.getMonth() + 1
+  const thisYear = now.getFullYear()
+
+  today.toString().length == 1 ? today = '0' + today : false
+  thisMonth.toString().length == 1 ? thisMonth = '0' + thisMonth : false
+
+  const nowHours = now.getUTCHours().toString().length == 1 ? '0' + now.getUTCHours() : now.getUTCHours()
+  const nowMinutes = now.getUTCMinutes().toString().length == 1 ? '0' + now.getUTCMinutes() : now.getUTCMinutes()
+  const nowSeconds = now.getUTCSeconds().toString().length == 1 ? '0' + now.getUTCSeconds() : now.getUTCSeconds()
+
+  return {
+    milliseconds: commafy(milliseconds),
+    seconds: commafy(seconds),
+    minutes: commafy(minutes),
+    hours: commafy(hours),
+    days: commafy(days),
+    weeks: commafy(weeks),
+    months: commafy(months),
+    years: commafy(years),
+    today: today,
+    thisMonth: thisMonth,
+    thisYear: thisYear,
+    nowHours: nowHours,
+    nowMinutes: nowMinutes,
+    nowSeconds: nowSeconds
+  }
+}
+
+commafy = (num) => {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+}
+
+module.exports = {
+  getAge
+}
